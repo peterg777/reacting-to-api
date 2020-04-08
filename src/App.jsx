b15826468c3e63830ab79cbda1eb84728ce8492e
peterg777 <ptrg777@gmail.com>
2020-04-08 16:25:21 -0400

    format document

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import FilmCard from './components/FilmCard';
 import PeopleCard from './components/PeopleCard';
 
+
 class App extends React.Component {
     state = {
         films: [],
@@ -34,31 +35,31 @@ class App extends React.Component {
                 </main>
             );
         } else if (this.state.isPeople || this.state.isFilms) {
-			return (
-				<main className="container">
-					<header className="d-flex">
-						<button type="button" onClick={this.handleHomePage} className="btn btn-succcess shadow my-3 mx-auto">Home</button>
-					</header>
-					<section className="row my-2 justify-content-center">
-						{this.state.people.map(person => <PeopleCard key={person.id} person={person} />)}
-					</section>
-				</main>
-			);
-		} else {
-			return (
-				<main className="container">
-					<section className="row my-3 flex-column align-items-center">
-						<img src="https://ghibliapi.herokuapp.com/images/logo.svg" alt="Studio Ghibli Logo" />
-						<div className="d-flex">
-							<button type="button" onClick={this.handleFilms} className="btn btn-warning shadow m-3">Films</button>
-							<button type="button" onClick={this.handlePeople} className="btn btn-warning shadow m-3">People</button>
-						</div>
-					</section>
-				</main>
-			);
-		}
-	}
+            return (
+                <main className="container">
+                    <header className="d-flex">
+                        <button type="button" onClick={this.handleHomePage} className="btn btn-succcess shadow my-3 mx-auto">Home</button>
+                    </header>
+                    <section className="row my-2 justify-content-center">
+                        {this.state.people.map(people => <PeopleCard key={people.id} people={people} />)}
+                    </section>
+                </main>
+            );
+        } else {
+            return (
+                <main className="container">
+                    <section className="row my-3 flex-column align-items-center">
+                        <img src="https://ghibliapi.herokuapp.com/images/logo.svg" alt="Studio Ghibli Logo" />
+                        <div className="d-flex">
+                            <button type="button" onClick={this.handleFilms} className="btn btn-warning shadow m-3">Films</button>
+                            <button type="button" onClick={this.handlePeople} className="btn btn-warning shadow m-3">People</button>
+                        </div>
+                    </section>
+                </main>
+            );
+        }
+    }
 }
 
 
-        export default App;
+export default App;
--- src/components/PeopleCard.jsx
+++ src/components/PeopleCard.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
-const PeopleCard = ({ person }) => {
-	return (
-		<article className="col-6">
-			<div className="card my-2 shadow">
-				<div className="card-body justify-content-center">
-					<h4 className="card-title">{person.name}</h4>
-					<p className="card-text">{person.age}</p>
-				</div>
-			</div>
-		</article>
-	);
+const PeopleCard = ({ people }) => {
+    return (
+        <article className="col-6">
+            <div className="card my-2 shadow">
+                <div className="card-body justify-content-center">
+                    <h4 className="card-title">{people.name}</h4>
+                    <p className="card-text">{people.age}</p>
+                </div>
+            </div>
+        </article>
+    );
 }
 
 export default PeopleCard;
